13e35a9004db211d4880fe3a7716a2e94fd595d5
Henrique Caricatti Capozzi <henriqueccapozzi@gmail.com>
2021-11-10 20:54:51 -0300

    Added favicon serving configuration

--- app/urls.py
+++ app/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic.base import RedirectView
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.contrib import admin
 
 from .views import home_view
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path("", home_view),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
